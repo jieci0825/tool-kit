0a814c28164725c6d4c02951e1d78b8fc726031e
coderjc <coderjc@qq.com>
2025-07-05 20:38:25 +0800

    feat: 完成 search-bar 布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const keyword = ref('')
+</script>
 
 <template>
     <div class="home-container">
@@ -7,9 +11,13 @@
             <div class="description">一个简单的工具箱</div>
         </div>
 
-        <div class="query-wrapper">
+        <div class="query-wrapper flex-center">
             <div class="search-bar">
-                <el-button type="primary">测试</el-button>
+                <el-input
+                    v-model="keyword"
+                    placeholder="请输入工具名称"
+                ></el-input>
+                <el-button type="primary">搜索</el-button>
             </div>
             <div class="category-list">
                 <div class="category-item">工具1</div>
@@ -29,7 +37,7 @@
     .header-wrapper {
         padding-top: 20px;
         .title {
-            font-size: 24px;
+            font-size: 22px;
             font-weight: bold;
             text-align: center;
             color: var(--title-primary-color);
@@ -43,8 +51,12 @@
     }
     .query-wrapper {
         margin: 30px auto;
+        flex-direction: column;
         .search-bar {
-            //
+            width: 50%;
+            display: flex;
+            align-items: center;
+            gap: 10px;
         }
     }
 }
